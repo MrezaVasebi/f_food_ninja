57fc558f2b96ff8b001966c8d04b32e002607c22
mrezavasebi <reza.vaseb@gmail.com>
2024-05-12 16:02:01 +0330

    refactored: splash widget refactored

--- lib/scr/presentation/splash.dart
+++ lib/scr/presentation/splash.dart
@@ -1,27 +1,30 @@
 import 'package:f_food_delivery/scr/presentation/constant/images.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            PngImages.backLogo,
-            fit: BoxFit.contain,
+    return Column(
+      children: [
+        Container(
+          height: 305,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(PngImages.backLogo),
+              fit: BoxFit.cover,
+            ),
           ),
-          const SizedBox(
-            height: 15,
-          ),
-          Image.asset(
-            PngImages.logo,
-          ),
-        ],
-      ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        Image.asset(
+          PngImages.logo,
+        )
+      ],
     );
   }
 }
