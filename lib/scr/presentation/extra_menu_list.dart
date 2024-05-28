c05baa0a7d1c2900ab0f068ea0933fcf578f6d1e
mrezavasebi <reza.vaseb@gmail.com>
2024-05-28 18:28:41 +0330

    added: extraMenu was added

--- lib/scr/presentation/extra_menu_list.dart
+++ lib/scr/presentation/extra_menu_list.dart
@@ -1,3 +1,8 @@
+import 'package:f_food_delivery/scr/presentation/constant/dummy_data.dart';
+import 'package:f_food_delivery/scr/presentation/menu_list.dart';
+import 'package:f_food_delivery/scr/presentation/widgets/app_text.dart';
+import 'package:f_food_delivery/scr/presentation/widgets/home_filter.dart';
+import 'package:f_food_delivery/scr/presentation/widgets/home_header.dart';
 import 'package:flutter/material.dart';
 
 class ExtraMenuList extends StatelessWidget {
@@ -6,7 +11,60 @@ class ExtraMenuList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
+      // color: Colors.red,
+      width: double.infinity,
       height: MediaQuery.of(context).size.height,
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: SizedBox(
+              // color: Colors.brown,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: [
+                  const HomeHeader(),
+                  const Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        HomeFilter(),
+                        SizedBox(height: 20),
+                        AppText(
+                          lbl: 'Popular Restaurant',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: SizedBox(
+                      height: double.infinity,
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 20,
+                          right: 20,
+                          bottom: 20,
+                        ),
+                        child: MediaQuery.removePadding(
+                            context: context,
+                            removeTop: true,
+                            child: MenuList(list: popular)),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
