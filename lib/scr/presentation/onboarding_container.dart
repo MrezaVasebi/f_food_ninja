321e09175b1a248f53d2949d700399deb55fb3bf
mrezavasebi <reza.vaseb@gmail.com>
2024-06-06 15:54:04 +0330

    fixed: ui problem fixed

--- lib/scr/presentation/onboarding_container.dart
+++ lib/scr/presentation/onboarding_container.dart
@@ -27,61 +27,68 @@ class _OnboardingContainerState extends State<OnboardingContainer> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 65,
-        ),
-        Image.asset(
-          width: 405,
-          height: 434,
-          onBoardingOne ? PngImages.imageOne : PngImages.imageTwo,
-        ),
-        const SizedBox(
-          height: 38,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 65,
-          ),
-          child: Center(
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      child: Expanded(
+        flex: 1,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 20),
             child: Column(
               children: [
-                AppText(
-                  lbl: onBoardingOne
-                      ? 'Find your Comfort Food here'
-                      : 'Food Ninja is Where Your Comfort Food Lives',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22,
+                const SizedBox(height: 65),
+                Image.asset(
+                  width: 405,
+                  height: 434,
+                  onBoardingOne ? PngImages.imageOne : PngImages.imageTwo,
+                ),
+                const SizedBox(height: 38),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 65,
                   ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                AppText(
-                  lbl: onBoardingOne
-                      ? 'Here You Can find a chef or dish for every taste and color. Enjoy!'
-                      : 'Enjoy a fast and smooth food delivery at your doorstep',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 15,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        AppText(
+                          lbl: onBoardingOne
+                              ? 'Find your Comfort Food here'
+                              : 'Food Ninja is Where Your Comfort Food Lives',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        AppText(
+                          lbl: onBoardingOne
+                              ? 'Here You Can find a chef or dish for every taste and color. Enjoy!'
+                              : 'Enjoy a fast and smooth food delivery at your doorstep',
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 42,
+                        ),
+                        if (!onBoardingOne)
+                          AppButton(
+                            lbl: 'Next',
+                            onPressed: () {},
+                          )
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 42,
-                ),
-                if (!onBoardingOne)
-                  AppButton(
-                    lbl: 'Next',
-                    onPressed: () {},
-                  )
+                )
               ],
             ),
           ),
-        )
-      ],
+        ),
+      ),
     );
   }
 }
